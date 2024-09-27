b8a87b6169777793d34bae8bab8b8b189b7adde4
Дюбрюкс Сергей <serhio5551>
2024-09-27 22:18:55 +0300

    Проект пересохранён после работы линтера black

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ while 1:
 card_example = "1122334455667788"
 account_example = "11122233344455566677"
 
-#print(f"Маска карты: {get_mask_card_number(card_num)}")
-#print(f"Маска счёта: {get_mask_account(account_num)}")
+# print(f"Маска карты: {get_mask_card_number(card_num)}")
+# print(f"Маска счёта: {get_mask_account(account_num)}")
 
 print(f"Маска карты: {get_mask_card_number(card_example)}")
+
 print(f"Маска счёта: {get_mask_account(account_example)}")
